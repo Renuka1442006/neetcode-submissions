33795720aaa624de261bb57747c23d623236a9fb
Renuka1442006 <renuka1442006@gmail.com>
2026-08-07 19:05:54 +0530

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+       HashMap<Character,Integer> map=new HashMap<>();
+       int maxlen=0;
+       int left=0;
+       for(int i=0;i<s.length();i++)
+       {
+            char ch=s.charAt(i);
+            if(map.containsKey(ch) && map.get(ch)>=left)
+            {
+                left=map.get(ch)+1;
+            }
+            map.put(ch,i);
+            maxlen=Math.max(maxlen,i-left+1);
+       }
+       return maxlen;
+    }
+}
